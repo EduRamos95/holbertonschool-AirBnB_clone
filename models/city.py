8d85f69a13a07740e8eca8b7b161e0c9e1469186
WilsonValer <nederig21@gmail.com>
2022-10-15 12:52:30 -0500

    was added new class city

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module city.py
+contain:
+    Class City inherits from Class BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ city class inherits from BaseModel
+     Public class attributes: state_id
+      Public class attributes: Name
+    """
+    state_id = ""
+    name = ""
