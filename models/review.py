b6737ad96d301c4a27b2249f18733d06a5f897be
WilsonValer <nederig21@gmail.com>
2022-10-15 12:54:18 -0500

    was added new class  review

---
+++ models/review.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module revie.py
+contain:
+    Class Revie inherits from Class BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Review class inherits from BaseModel
+    Public class attributes: place_id
+    Public class attributes: user_id
+    Public class attributes: text
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
