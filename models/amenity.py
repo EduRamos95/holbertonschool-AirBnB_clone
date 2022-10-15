610cf2427915dae8657ae2d59ffc74bedcdc580d
WilsonValer <nederig21@gmail.com>
2022-10-15 12:53:13 -0500

    was added new class amenity

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module Amenity.py
+contain:
+    Class Amenity inherits from Class BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    amenity: inherits from BaseModel
+    Public class attributes: Name
+    """
+    name = ""
