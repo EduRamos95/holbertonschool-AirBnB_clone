293de085d1cf044cfb9c426b4a9bfeb6b4a2ce88
WilsonValer <nederig21@gmail.com>
2022-10-15 12:49:54 -0500

    added new class state

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+ """
+ Module state
+ contain:
+     Class State inherits from Class BaseModel
+ """
+ from models.base_model import BaseModel
+
+
+ class State(BaseModel):
+     """State: inherits from BaseModel
+     Public class attributes: Name
+     """
+     name = ""
